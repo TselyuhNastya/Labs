c54e6e65a183be11c9761989e8f1d39a5f5a4d6d
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-05-03 10:36:42 +0300

    массив книг

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лев Толстой',
+        Name: 'Война и мир',
+        publicationYear: 1869,
+        price: 1200,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Федор Достоевский',
+        Name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 950,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Айн Рэнд',
+        Name: 'Атлант расправил плечи',
+        publicationYear: 1957,
+        price: 1500,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Джек Лондон',
+        Name: 'Белый Клык',
+        publicationYear: 1906,
+        price: 700,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        Name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 1100,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        Name: '1984',
+        publicationYear: 1949,
+        price: 800,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Мария Цветаева',
+        Name: 'Избранное',
+        publicationYear: 1920,
+        price: 600,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Гюстав Флобер',
+        Name: 'Госпожа Бовари',
+        publicationYear: 1857,
+        price: 850,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Харуки Мураками',
+        Name: 'Кафка на пляже',
+        publicationYear: 2002,
+        price: 650,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джон Толкин',
+        Name: 'Властелин колец',
+        publicationYear: 1954,
+        price: 2000,
+        bestseller: true,
+        available: true
+    }
+];
